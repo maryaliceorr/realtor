496d11d88710f97114a42142a394e6e7fc212361
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-26 19:11:58 -0500

    added components

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
 
+import Building from '../components/Building/Building';
+
+import ListingForm from '../components/ListingForm/ListingForm';
+
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Listings />
+      <div className="App">
+        <Listings />
+        <Building />
+        <ListingForm />
+      </div>
     );
   }
 }
