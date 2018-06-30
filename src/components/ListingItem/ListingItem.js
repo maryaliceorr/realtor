b742895a853edd503fb696f72091ad614be712f9
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-30 11:20:33 -0500

    wip commit

---
+++ src/components/ListingItem/ListingItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {listingShape} from '../../propz/listingProp';
+import './ListingItem.css';
+
+class ListingItem extends React.Component {
+  static propTypes = {
+    listing: listingShape,
+  }
+  render () {
+    const {listing} = this.props;
+    return (
+      <li>{listing.price}</li>
+    );
+  }
+}
+
+export default ListingItem;
